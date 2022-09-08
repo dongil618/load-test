671660400312875f2b2429341192f5a742953789
dongil618 <rlaehddlf618@gmail.com>
2022-09-08 16:41:57 +0900

    Chore : 프로젝트 생성

---
+++ src/test/java/com/hanghae99/loadtest/LoadTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae99.loadtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
