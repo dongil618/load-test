64855a2bcebeafac76c6dcaa1b490f05ff84ebca
dongil618 <rlaehddlf618@gmail.com>
2022-09-09 14:29:06 +0900

    Feat : 동적 페이지

--- src/main/java/com/hanghae99/loadtest/controller/IndexController.java
+++ src/main/java/com/hanghae99/loadtest/controller/IndexController.java
@@ -1,7 +1,10 @@
 package com.hanghae99.loadtest.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Date;
 
 @Controller
 public class IndexController {
@@ -9,5 +12,11 @@ public class IndexController {
     @GetMapping("/")
     public String home(){
         return "index";
+    }
+
+    @GetMapping("/time")
+    public String time(Model model) {
+        model.addAttribute("time", new Date());
+        return "time";
     }
 }
